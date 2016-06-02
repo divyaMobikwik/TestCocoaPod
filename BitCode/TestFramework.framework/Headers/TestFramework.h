7accd389dd1f6da3159535c79dc68cfa2565f709
Akashdeep <akashdeep@mobikwik.com>
2016-06-02 16:12:26 +0530

    Adding Bitcode enabled build in it.

---
+++ BitCode/TestFramework.framework/Headers/TestFramework.h
@@ -0,0 +1,22 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Akashdeep on 16/12/15.
+//  Copyright © 2015 MobiKwik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <TestFramework/TestImportClass1.h>
+
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
+ 
---
+++ BitCode/TestFramework.framework/Headers/TestImportClass1.h
@@ -0,0 +1,16 @@
+//
+//  TestImportClass1.h
+//  TestFramework
+//
+//  Created by Akashdeep on 30/03/16.
+//  Copyright © 2016 MobiKwik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TestImportClass2.h"
+
+@interface TestImportClass1 : NSObject
+
+-(void)printSomething;
+
+@end
